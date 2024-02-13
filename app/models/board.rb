8a2b9e7dc9f2159698c6445ce2c41e2be2e8a8c0
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-02-13 15:01:50 +0000

    Added board and post function

--- app/models/board.rb
+++ app/models/board.rb
@@ -8,4 +8,6 @@
 #  updated_at :datetime         not null
 #
 class Board < ApplicationRecord
+  has_many(:posts, class_name: "Post", foreign_key: "board_id")
+  # has_many(:active_posts, -> {where(expires_on < datetime)})
 end
