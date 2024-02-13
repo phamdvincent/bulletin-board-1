c4b68400457ffa89ff84fb4e5f40f641c290ac5d
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-02-13 07:39:31 +0000

    Added board index and show

--- config/routes.rb
+++ config/routes.rb
@@ -40,5 +40,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  get("/", { :controller => "boards", :action => "index" })
   # root "articles#index"
 end
